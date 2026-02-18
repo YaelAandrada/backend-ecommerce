e1cb09d98f90b4f1a510ed37ae3a07c15134eb68
ana <laboralanaluz.a.b@gmail.com>
2026-02-17 22:45:41 -0300

    Variables de entorno

---
+++ .env/server.js
@@ -0,0 +1,20 @@
+// Server
+PORT=5000
+NODE_ENV=development
+
+// Database
+MONGODB_URI=mongodb+srv://ruiz_db_user:<user123>@cluster0.zfdkhin.mongodb.net
+
+// Email (Nodemailer)
+EMAIL_HOST=smtp.gmail.com
+EMAIL_PORT=587
+EMAIL_USER="tuemail@gmail.com";
+EMAIL_PASS=tu_contraseña_app
+
+// Cloudinary (para imágenes)
+CLOUDINARY_CLOUD_NAME=tu_cloud_name
+CLOUDINARY_API_KEY=tu_api_key
+CLOUDINARY_API_SECRET=tu_api_secret
+
+// Frontend URL
+FRONTEND_URL="http://localhost:3000"
