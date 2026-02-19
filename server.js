23a95db4048c87f98de37e39c81e0a8b64c95b26
NahuelQuesada <nahueldiazquesada@gmail.com>
2026-02-19 17:28:52 -0300

    Fixeando

--- server.js
+++ server.js
@@ -43,16 +43,3 @@ app.listen(PORT, () => {
 
 
 
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-
-
-mongoose.connect("mongodb+srv://ruiz_db_user:user123@cluster0.zfdkhin.mongodb.net/ecommerce")
-  .then(() => console.log("MongoDB Atlas conectado"))
-  .catch((err) => console.log("Error MongoDB:", err));
-
-app.listen(3000, () => {
-  console.log("Backend corriendo en puerto 3000");
-});
