95ca5cabc0d946419f7a2d5ffd6b7640a1049ef3
NahuelQuesada <nahueldiazquesada@gmail.com>
2026-02-19 01:28:29 -0300

    Fix Api (4)

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
 import authRoutes from "./routes/auth.routes.js";
 
-const app = express();
+
 
 app.use(cors());
 app.use(express.json());
